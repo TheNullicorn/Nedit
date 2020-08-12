61e39c08171c9fb8d81a7033881e8697864985ab
Nullicorn <bennullicorn@gmail.com>
2020-08-12 12:36:31 -0400

    Add complete docs for NBTCompound

--- src/main/java/me/nullicorn/nedit/type/NBTCompound.java
+++ src/main/java/me/nullicorn/nedit/type/NBTCompound.java
@@ -5,41 +5,90 @@ import java.util.Map;
 import org.jetbrains.annotations.Nullable;
 
 /**
+ * Represents an NBT compound tag ({@link TagType#COMPOUND})
+ *
  * @author Nullicorn
  */
 public class NBTCompound extends HashMap<String, Object> {
 
+    /**
+     * @param key          A dot-separated path to the desired field
+     * @param defaultValue The value to return if the field does not exist
+     * @return The double at the desired path, or the default value if it does not exist
+     * @see #get(String)
+     */
     public double getDouble(String key, double defaultValue) {
         return getNumber(key, defaultValue).doubleValue();
     }
 
+    /**
+     * @param key          A dot-separated path to the desired field
+     * @param defaultValue The value to return if the field does not exist
+     * @return The floating-point number at the desired path, or the default value if it does not exist
+     * @see #get(String)
+     */
     public float getFloat(String key, float defaultValue) {
         return getNumber(key, defaultValue).floatValue();
     }
 
+    /**
+     * @param key          A dot-separated path to the desired field
+     * @param defaultValue The value to return if the field does not exist
+     * @return The short at the desired path, or the default value if it does not exist
+     * @see #get(String)
+     */
     public short getShort(String key, short defaultValue) {
         return getNumber(key, defaultValue).shortValue();
     }
 
+    /**
+     * @param key          A dot-separated path to the desired field
+     * @param defaultValue The value to return if the field does not exist
+     * @return The long at the desired path, or the default value if it does not exist
+     * @see #get(String)
+     */
     public long getLong(String key, long defaultValue) {
         return getNumber(key, defaultValue).longValue();
     }
 
+    /**
+     * @param key          A dot-separated path to the desired field
+     * @param defaultValue The value to return if the field does not exist
+     * @return The integer at the desired path, or the default value if it does not exist
+     * @see #get(String)
+     */
     public int getInt(String key, int defaultValue) {
         return getNumber(key, defaultValue).intValue();
     }
 
+    /**
+     * @param key          A dot-separated path to the desired field
+     * @param defaultValue The value to return if the field does not exist
+     * @return The byte at the desired path, or the default value if it does not exist
+     * @see #get(String)
+     */
     public byte getByte(String key, byte defaultValue) {
         return getNumber(key, defaultValue).byteValue();
     }
 
-    public Number getNumber(String key, Number defaultValue) {
+    /**
+     * @param key          A dot-separated path to the desired field
+     * @param defaultValue The value to return if the field does not exist
+     * @return The number at the desired path, or the default value if it does not exist
+     * @see #get(String)
+     */
+    public Number getNumber(String key, @Nullable Number defaultValue) {
         Object result = get(key);
         return result instanceof Number
             ? (Number) result
             : defaultValue;
     }
 
+    /**
+     * @param key A dot-separated path to the desired field
+     * @return The string at the desired path, or null if it does not exist
+     * @see #get(String)
+     */
     @Nullable
     public String getString(String key, String defaultValue) {
         Object result = get(key);
@@ -48,6 +97,11 @@ public class NBTCompound extends HashMap<String, Object> {
             : defaultValue;
     }
 
+    /**
+     * @param key A dot-separated path to the desired field
+     * @return The long array at the desired path, or null if it does not exist
+     * @see #get(String)
+     */
     @Nullable
     public Long[] getLongArray(String key) {
         Object result = get(key);
@@ -56,6 +110,11 @@ public class NBTCompound extends HashMap<String, Object> {
             : null;
     }
 
+    /**
+     * @param key A dot-separated path to the desired field
+     * @return The integer array at the desired path, or null if it does not exist
+     * @see #get(String)
+     */
     @Nullable
     public Integer[] getIntArray(String key) {
         Object result = get(key);
@@ -64,6 +123,11 @@ public class NBTCompound extends HashMap<String, Object> {
             : null;
     }
 
+    /**
+     * @param key A dot-separated path to the desired field
+     * @return The byte array at the desired path, or null if it does not exist
+     * @see #get(String)
+     */
     @Nullable
     public Byte[] getByteArray(String key) {
         Object result = get(key);
@@ -72,6 +136,11 @@ public class NBTCompound extends HashMap<String, Object> {
             : null;
     }
 
+    /**
+     * @param key A dot-separated path to the desired field
+     * @return The list at the desired path, or null if it does not exist
+     * @see #get(String)
+     */
     @Nullable
     public NBTList getList(String key) {
         Object result = get(key);
@@ -80,6 +149,11 @@ public class NBTCompound extends HashMap<String, Object> {
             : null;
     }
 
+    /**
+     * @param key A dot-separated path to the desired field
+     * @return The compound at the desired path, or null if it does not exist
+     * @see #get(String)
+     */
     @Nullable
     public NBTCompound getCompound(String key) {
         Object result = get(key);
@@ -88,6 +162,32 @@ public class NBTCompound extends HashMap<String, Object> {
             : null;
     }
 
+    /**
+     * Retrieve a value from within this compound.
+     * <p>
+     * <p>
+     * Consider the following NBT data:
+     * <pre>{@code {
+     *   user: {
+     *     name: "User291",
+     *     id: "ae630aa9-cdbb-4aaf-be42-868889585b4d",
+     *     tokens: 19531L,
+     *     socialMedia: {
+     *       discord: "Player291#4444",
+     *       twitter: "User291"
+     *     }
+     *   }
+     * }}</pre>
+     * In this case, to acces the user's <b>Discord username</b>, we'd need the path:
+     * <pre>{@code "user.socialMedia.discord"}</pre>
+     * To access the <b>user's ID</b>, we could use the path:
+     * <pre>{@code "user.id"}</pre>
+     * And to access the <b>whole user</b> object, we could just use the path:
+     * <pre>{@code "user"}</pre>
+     *
+     * @param key A dot-separated path to the desired field (see above example)
+     * @return The value at the specified path, or null if the field does not exist
+     */
     @Nullable
     public Object get(String key) {
         if (key == null || key.isEmpty()) {
