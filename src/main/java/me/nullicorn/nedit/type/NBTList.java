eed71d226affd55dcc8fd24784c07f0e202edcc4
Nullicorn <bennullicorn@gmail.com>
2020-08-14 14:58:25 -0400

    Add get and forEach methods for each tag type

--- src/main/java/me/nullicorn/nedit/type/NBTList.java
+++ src/main/java/me/nullicorn/nedit/type/NBTList.java
@@ -2,7 +2,9 @@ package me.nullicorn.nedit.type;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.ConcurrentModificationException;
 import java.util.Objects;
+import java.util.function.Consumer;
 import lombok.Getter;
 
 /**
@@ -50,6 +52,259 @@ public class NBTList extends ArrayList<Object> {
     public boolean addAll(int index, Collection<?> c) {
         c.forEach(this::checkType);
         return super.addAll(index, c);
+    }
+
+    /*
+     *
+     * ============ GETTER METHODS ============
+     *
+     */
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain byte tags
+     */
+    public Byte getByte(int index) {
+        checkGetType(TagType.BYTE);
+        return (Byte) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain short tags
+     */
+    public Short getShort(int index) {
+        checkGetType(TagType.SHORT);
+        return (Short) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain integer tags
+     */
+    public Integer getInt(int index) {
+        checkGetType(TagType.INT);
+        return (Integer) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain long tags
+     */
+    public Long getLong(int index) {
+        checkGetType(TagType.LONG);
+        return (Long) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain float tags
+     */
+    public Float getFloat(int index) {
+        checkGetType(TagType.FLOAT);
+        return (Float) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain double tags
+     */
+    public Double getDouble(int index) {
+        checkGetType(TagType.DOUBLE);
+        return (Double) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain string tags
+     */
+    public String getString(int index) {
+        checkGetType(TagType.STRING);
+        return (String) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain byte array tags
+     */
+    public Byte[] getByteArray(int index) {
+        checkGetType(TagType.BYTE_ARRAY);
+        return (Byte[]) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain integer array tags
+     */
+    public Integer[] getIntArray(int index) {
+        checkGetType(TagType.INT_ARRAY);
+        return (Integer[]) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain long array tags
+     */
+    public Long[] getLongArray(int index) {
+        checkGetType(TagType.LONG_ARRAY);
+        return (Long[]) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain NBT list tags
+     */
+    public NBTList getList(int index) {
+        checkGetType(TagType.LIST);
+        return (NBTList) get(index);
+    }
+
+    /**
+     * @throws IllegalStateException If this this list cannot contain NBT compound tags
+     */
+    public NBTCompound getCompound(int index) {
+        checkGetType(TagType.COMPOUND);
+        return (NBTCompound) get(index);
+    }
+
+    /*
+     *
+     * ============ FOREACH METHODS ============
+     *
+     */
+
+    /**
+     * Iterate through each byte in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain byte tags
+     */
+    public void forEachByte(Consumer<Byte> action) {
+        checkGetType(TagType.BYTE);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each short in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain short tags
+     */
+    public void forEachShort(Consumer<Short> action) {
+        checkGetType(TagType.SHORT);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each integer in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain integer tags
+     */
+    public void forEachInt(Consumer<Integer> action) {
+        checkGetType(TagType.INT);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each long in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain long tags
+     */
+    public void forEachLong(Consumer<Long> action) {
+        checkGetType(TagType.LONG);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each float in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain float tags
+     */
+    public void forEachFloat(Consumer<Float> action) {
+        checkGetType(TagType.FLOAT);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each double in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain double tags
+     */
+    public void forEachDouble(Consumer<Double> action) {
+        checkGetType(TagType.DOUBLE);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each string in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain string tags
+     */
+    public void forEachString(Consumer<String> action) {
+        checkGetType(TagType.STRING);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each byte array in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain byte array tags
+     */
+    public void forEachByteArray(Consumer<Byte[]> action) {
+        checkGetType(TagType.BYTE_ARRAY);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each integer array in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain integer array tags
+     */
+    public void forEachIntArray(Consumer<Integer[]> action) {
+        checkGetType(TagType.INT_ARRAY);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each long array in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain long array tags
+     */
+    public void forEachLongArray(Consumer<Long[]> action) {
+        checkGetType(TagType.LONG_ARRAY);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each NBT list in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain list tags
+     */
+    public void forEachList(Consumer<NBTList> action) {
+        checkGetType(TagType.LIST);
+        forEachOfType(action);
+    }
+
+    /**
+     * Iterate through each NBT compound in this list
+     *
+     * @throws IllegalStateException If this this list cannot contain compound tags
+     */
+    public void forEachCompound(Consumer<NBTCompound> action) {
+        checkGetType(TagType.COMPOUND);
+        forEachOfType(action);
+    }
+
+    /**
+     * Same as {@link #forEach(Consumer)} but with a separate type parameter
+     */
+    private <T> void forEachOfType(Consumer<T> action) {
+        Objects.requireNonNull(action);
+        final int expectedModCount = modCount;
+
+        @SuppressWarnings("unchecked") final T[] elementData = (T[]) this.toArray();
+        for (int i = 0; modCount == expectedModCount && i < this.size(); i++) {
+            action.accept(elementData[i]);
+        }
+        if (modCount != expectedModCount) {
+            throw new ConcurrentModificationException();
+        }
+    }
+
+    /**
+     * Throw an exception if the specified type is not contained in this list
+     */
+    private void checkGetType(TagType attemptedType) {
+        if (attemptedType != contentType) {
+            throw new IllegalStateException("Cannot get " + attemptedType + " from a list of " + contentType.toString() + "s");
+        }
     }
 
     /**
