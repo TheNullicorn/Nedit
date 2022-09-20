d9ef84d2e018839a549ac11a0aa054a1f39bcb1f
Nullicorn <bennullicorn@gmail.com>
2022-09-20 18:46:05 -0400

    Switch from Maven to Gradle
    - Delete pom.xml
    - Add gradle wrapper
    - Add build script (build.gradle.kts)
    - Use version catalog for managing plugins & libraries
    - Reconfigure maven publishing
    - Switch from javadoc to dokka
      - Much easier on the eyes
      - Requires Kotlin's JVM plugin, which we're otherwise not using

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    // Publishing to Maven Central
+    id("java")
+    id("signing")
+    id("maven-publish")
+
+    // Documentation
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.dokka)
+}
+
+group = "me.nullicorn"
+version = "2.2.0-SNAPSHOT"
+description = property("description") as String
+
+// Must go after group & version or else it won't be able to access them.
+apply(from = "gradle/publish.gradle.kts")
+
+repositories {
+    mavenLocal()
+    maven("https://repo.maven.apache.org/maven2/")
+}
+
+dependencies {
+    // Use JUnit Jupiter for unit testing
+    testImplementation(libs.junit)
+
+    // Use Java syntax instead of Kotlin when documenting method & class signatures with Dokka.
+    dokkaHtmlPlugin(libs.dokka.plugin.java)
+}
+
+// Use Java 8
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+// Source files are encoded using UTF-8
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+// Use JUnit for running tests
+tasks.test {
+    useJUnitPlatform()
+}
